15826feddaa730746247588c5c100c54a7ecfc12
JY <123rbdud@gmail.com>
2021-12-02 09:34:26 +0900

    create project

---
+++ vuedongsan/src/App.vue
@@ -0,0 +1,115 @@
+<template>
+
+<div class="menu">
+  <a v-for="작명 in 메뉴들" :key=작명>{{ 작명 }}</a>
+</div>
+
+<div class="black-bg" v-if="모달창열렸니 == true">
+  <div class="white-bg">
+    <h4>상세페이지</h4>
+    <p>상세페이지내용임</p>
+    <button @click="모달창열렸니 = false">닫기</button>
+  </div>
+</div>
+
+
+<div v-for="(a,i) in products" :key="i">
+  <img src="./assets/room0.jpg" class="room-img">
+  <h4 @click="모달창열렸니 = true"> {{products[i]}}</h4>
+  <p>50만원</p>
+    <button @click="신고수[i]++">허위매물신고</button>
+  <span>신고수 : {{신고수[i]}}</span>
+
+</div>
+
+
+<!-- <div>
+  <img src="./assets/room0.jpg" class="room-img">
+  <h4 @click="모달창열렸니 = true"> {{products[0]}}</h4>
+  <p>50만원</p>
+    <button @click="신고수[0]++">허위매물신고</button>
+  <span>신고수 : {{신고수[0]}}</span>
+</div>
+
+<div>
+  <img src="./assets/room1.jpg" class="room-img">
+  <h4>{{products[1]}}</h4>
+  <p>50만원</p>
+    <button @click="신고수[1]++">허위매물신고</button>
+  <span>신고수 : {{신고수[1]}}</span>
+</div>
+
+<div>
+  <img src="./assets/room2.jpg" class="room-img">
+  <h4>{{products[2]}}</h4>
+  <p>50만원</p>
+    <button @click="신고수[2]++">허위매물신고</button>
+  <span>신고수 : {{신고수[2]}}</span>
+</div> -->
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      모달창열렸니: false,
+      메뉴들: ['Home', 'Shop', 'About'],
+      products: ['역삼동', '천호동', '마포구'],
+      신고수: [0,0,0]
+    }
+  },
+
+  methods : {
+    increase(){
+      this.신고수 += 1;
+    }
+
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
+.room-img{
+  width: 100%;
+  margin-top: 40px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.menu {
+  background : darkslateblue;
+  padding : 15px;
+  border-radius : 5px;
+}
+.menu a {
+  color : white;
+  padding : 10px;
+}
+</style>
